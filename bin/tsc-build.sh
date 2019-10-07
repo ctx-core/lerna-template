30b0f8845c6c948e9440bc16592e7b954e531592
Brian Takita <brian.takita@gmail.com>
2019-10-07 10:20:41 -0400

    + bin/tsc-build.sh

---
+++ bin/tsc-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ROOT_DIR=$(dirname $(dirname "$0"))
+#tsc -b \
+#  $(ls $ROOT_DIR/packages/ctx-core/packages/*/tsconfig.json | xargs dirname) \
+#  $(ls $ROOT_DIR/packages/*/tsconfig.json | grep -v ctx-core | xargs dirname) \
+#  $@
+tsc -b \
+  $(ls $ROOT_DIR/packages/*/tsconfig.json | xargs dirname) \
+  $@
