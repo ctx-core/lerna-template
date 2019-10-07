93e3d6c1f3df75e1927374b65bc30d723c651569
Brian Takita <brian.takita@gmail.com>
2019-10-07 12:03:37 -0400

    bin/tmux-dev.sh: + $SHELL
    ∋ "typescript": "^3.6.3"

--- bin/tmux-dev.sh
+++ bin/tmux-dev.sh
@@ -3,25 +3,22 @@ DEV_DIR_DEFAULT=~/work/dev/
 DEV_DIR="${DEV_DIR:-$DEV_DIR_DEFAULT}"
 
 cd $DEV_DIR
-
 tmux rename-window dev
-tmux split-window -v
+tmux split-window -v $SHELL
 tmux send-keys 'tig' 'C-m'
 tmux select-pane -t 0
 
 cd $DEV_DIR
-
-tmux new-window
+tmux new-window $SHELL
 tmux rename-window build-typescript
 tmux send-keys 'tsc-build.sh -w' 'C-m'
 tmux select-pane -t 0
 
 cd $DEV_DIR/packages/_web
-
-tmux new-window
+tmux new-window $SHELL
 tmux rename-window web
 tmux send-keys 'yarn run dev' 'C-m'
-tmux split-window -v
+tmux split-window -v $SHELL
 tmux select-layout even-vertical
 tmux select-pane -t 0
 
